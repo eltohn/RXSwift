ac84dbd2b93e370ab51e5ba3562eb5628a4755b2
el <victoriousserious@gmail.com>
2021-11-12 20:16:56 +0500

    12Nov

--- IntroToRXSwift/ViewController.swift
+++ IntroToRXSwift/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    
+    private let viewLabel: UILabel = {
+        let l = UILabel()
+        l.text = "El"
+        return l
+    }()
+    
+    var names = BehaviorRelay(value: ["Elbek"])
+    let bag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        view.addSubview(viewLabel)
+        viewLabel.frame = CGRect(x: 10, y: 10, width: view.frame.width, height: view.frame.height)
+        
+        
+        names.asObservable()
+            .subscribe(onNext:{ [weak self] value in
+                self?.viewLabel.text = value.joined(separator: ", ")
+            }).disposed(by: bag)
+        
+        names.accept(["a","b","c","d","e","f"])
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+            self.names.accept(["g","h","i"])
+        }
     }
-
-
 }
 
